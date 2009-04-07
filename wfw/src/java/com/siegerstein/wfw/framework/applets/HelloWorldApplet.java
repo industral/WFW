783d93f08b2cbea56204104db708918cd7755188
Alex Ivasyuv <industral@gmail.com>
2009-04-07 23:07:47 +0300

    - add applet

---
+++ wfw/src/java/com/siegerstein/wfw/framework/applets/HelloWorldApplet.java
@@ -0,0 +1,38 @@
+package com.siegerstein.wfw.framework.applets;
+
+import java.awt.EventQueue;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+
+/**
+ *Simple Hello World applet.
+ * @author Alex Ivasyuv
+ */
+public class HelloWorldApplet extends JApplet {
+
+  // --------------------------------------------------------------------
+  // Public methods
+  // --------------------------------------------------------------------
+
+  /**
+   * Main applet method.
+   */
+  public final void init() {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        JLabel label = new JLabel("Hello World!");
+        add(label);
+      }
+    });
+  }
+
+  // --------------------------------------------------------------------
+  // Private variables
+  // --------------------------------------------------------------------
+
+  /**
+   * UUID.
+   */
+  private static final long serialVersionUID = -5253459980406551544L;
+}
