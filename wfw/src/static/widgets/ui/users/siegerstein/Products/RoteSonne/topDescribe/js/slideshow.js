dcd6c41f4aa848e869bb5cc371b1d475cd249c7a
Alex Ivasyuv <>
2009-09-16 01:57:34 +0300

    - finish work on slideshow

--- wfw/src/static/widgets/ui/users/siegerstein/Products/RoteSonne/topDescribe/js/slideshow.js
+++ wfw/src/static/widgets/ui/users/siegerstein/Products/RoteSonne/topDescribe/js/slideshow.js
@@ -1,17 +1,18 @@
 function SlideShow() {
   var self = this;
+  const effect = "scale";
+  const timeEffect = 10000;
 
   this.currentAsset = 1;
   this.assetsArray = $("#slideShow").children().children();
   this.assetsCount = this.assetsArray.length;
 
   this.start = function() {
-    $('#slideShow li').removeClass('show').addClass('hide');
-    $(self.assetsArray[0]).removeClass('hide').addClass('show');
-    
+    $(self.assetsArray[0]).addClass('show').show(effect);
+
     setInterval(function() {
       self.next();
-    }, 5000);
+    }, timeEffect);
   }
 
   this.next = function() {
@@ -19,8 +20,26 @@ function SlideShow() {
       self.currentAsset = 0;
     }
 
-    $('#slideShow li').removeClass('show').addClass('hide');
-    $(self.assetsArray[self.currentAsset++]).removeClass('hide').addClass('show');
+    if (self.currentAsset == 0) {
+      if ($(self.assetsArray[self.assetsCount - 1]).hasClass('show')) {
+        this.prevAsset = self.assetsCount - 1;
+      } else {
+        this.prevAsset = 0;
+      }
+    } else {
+      this.prevAsset = self.currentAsset - 1;
+    }
+
+    if ($(self.assetsArray[this.prevAsset]).hasClass('show')) {
+      $(self.assetsArray[this.prevAsset]).removeClass('show').hide(
+          effect,
+          {},
+          null,
+          function() {
+            $(self.assetsArray[self.currentAsset++]).addClass('show').show(
+                effect);
+          });
+    }
   }
 }
 
